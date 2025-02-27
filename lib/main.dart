81a6b0965cb0d594da725ee685f61ea0452df413
Martin Pilný <martin.pilnas@email.cz>
2025-02-27 19:06:58 +0100

    přihlášení

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:bakalarska_prace_pilny/registration.dart';
 import 'package:flutter/material.dart';
 import 'language_mapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,14 +37,80 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final String language;
 
   const LoginPage({required this.language, super.key});
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController usernameController;
+  late TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    usernameController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> login() async {
+    try {
+      final hashedPassword =
+          sha256.convert(utf8.encode(passwordController.text)).toString();
+
+      DatabaseReference usersRef = FirebaseDatabase.instance.ref('users');
+      DatabaseEvent event =
+          await usersRef
+              .orderByChild('username')
+              .equalTo(usernameController.text)
+              .once();
+
+      if (event.snapshot.value == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('No user found for that username.')),
+        );
+        return;
+      }
+
+      // Get the user data
+      Map<dynamic, dynamic> userData =
+          (event.snapshot.value as Map).values.first;
+
+      // Check if the password matches
+      if (userData['password'] != hashedPassword) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Incorrect password.')));
+        return;
+      } else {
+        // Navigate to the LevelLanguagePage
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => LevelLanguagePage(language: widget.language),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('An error occurred: $e')));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final languageMapper = LanguageMapper(language);
+    final languageMapper = LanguageMapper(widget.language);
 
     return Scaffold(
       body: BackgroundGradient(
@@ -64,6 +133,7 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(height: 40),
                 // Username Field
                 TextField(
+                  controller: usernameController,
                   decoration: InputDecoration(
                     labelText: languageMapper.getTitle('username'),
                     hintText: 'Jan123',
@@ -75,6 +145,7 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(height: 20),
                 // Password Field
                 TextField(
+                  controller: passwordController,
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: languageMapper.getTitle('password'),
@@ -86,13 +157,7 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(height: 30),
                 // Login Button
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => LevelLanguagePage(language: language),
-                      ),
-                    );
-                  },
+                  onPressed: login,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                       vertical: 12,
@@ -110,7 +175,8 @@ class LoginPage extends StatelessWidget {
                   onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
-                        builder: (_) => RegistrationPage(language: language),
+                        builder:
+                            (_) => RegistrationPage(language: widget.language),
                       ),
                     );
                   },
